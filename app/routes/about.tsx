5b04738e61b6050711ef182f096d02d00564be16
Ivin Mathew Kurian <xavenger2410@gmail.com>
2025-11-04 11:12:57 +0530

    Setting up screen transitions using observer

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react"
-import {gsap} from "gsap";
+import { gsap } from "gsap";
 import { Observer } from "gsap/all";
 import { useGSAP } from "@gsap/react";
 
@@ -15,85 +15,112 @@ const about = (props: Props) => {
     const circleRef = useRef<HTMLDivElement | null>(null);
     const boxRef1 = useRef<HTMLDivElement | null>(null);
     const boxRef2 = useRef<HTMLDivElement | null>(null);
-    
-    
+
+    let originalColor = "";
+
+    useEffect(()=>{
+        originalColor = circleRef.current?.style.backgroundColor || "";
+    },[])
+
 
     function onMove(e: MouseEvent) {
-        setMouse({x:e.clientX, y:e.clientY});
+        setMouse({ x: e.clientX, y: e.clientY });
     }
 
-    function onScroll(e: WheelEvent){
+    function onScroll(e: WheelEvent) {
         const lineHeight = 16;
         const pageHeight = window.innerHeight || 800;
 
-        const scale = e.deltaMode ===1 ? lineHeight :
+        const scale = e.deltaMode === 1 ? lineHeight :
             e.deltaMode === 2 ? pageHeight : 2;
 
         const deltaPixels = e.deltaY * scale;
 
-        setScroll(scroll+deltaPixels);
+        setScroll(scroll + deltaPixels);
     }
 
-    function changeColor( colour : string){
-        gsap.to(circleRef.current,{
-            background:colour,
+    function changeColor(colour: string) {
+        gsap.to(circleRef.current, {
+            backgroundColor: colour,
         })
     }
 
-    useGSAP(()=>{
-        gsap.to(circleRef.current,{
-            x:mouse.x,
-            y:mouse.y,
-            ease:"power2.out",
-            duration:0.3,
+    useGSAP(() => {
+        gsap.to(circleRef.current, {
+            x: mouse.x,
+            y: mouse.y,
+            ease: "power2.out",
+            duration: 0.3,
         })
+    }, [mouse]);
+
+    useGSAP(() => {
 
         Observer.create({
-            target:boxRef1.current,
-            type:"pointer",
-            onHover: () =>{
-                console.log("Testing OnHover prop for box 1");
+            target: boxRef1.current,
+            type: "pointer",
+            onHover: () => {
+                console.log("Testing OnHover prop for box 1. Current Color: ",getComputedStyle(boxRef1.current).backgroundColor);
                 changeColor(getComputedStyle(boxRef1.current).backgroundColor);
+            },
+            onHoverEnd: () =>{
+                console.log("Hover end for box ref 1");
+                changeColor(originalColor);
             }
         })
 
         Observer.create({
-            target:boxRef2.current,
-            type:"pointer",
-            onHover: () =>{
-                console.log("Testing OnHover prop for box 2");
+            target: boxRef2.current,
+            type: "pointer",
+            onHover: () => {
+                console.log("Testing OnHover prop for box 2. Current Color: ",getComputedStyle(boxRef2.current).backgroundColor);
                 changeColor(getComputedStyle(boxRef2.current).backgroundColor);
+            },
+            onHoverEnd: () =>{
+                console.log("Hover end for box ref 2");
+                changeColor(originalColor);
             }
         })
-    },[mouse]);
 
-    
+        Observer.create({
+            // type:"wheel",
+            onDown: ()=>{
+                console.log("Scrolled down");
+            },
+            onUp: ()=>{
+                console.log("Scrolled up");
+            }
+        })
+
+    })
+
+
 
     useEffect(() => {
-        window.addEventListener("mousemove",onMove);
-        window.addEventListener("wheel",onScroll);
-        return() =>{
+        window.addEventListener("mousemove", onMove);
+        window.addEventListener("wheel", onScroll);
+        return () => {
             window.removeEventListener("mousemove", onMove);
             window.removeEventListener("wheel", onScroll);
         }
     }, [])
 
     return (
-        <div className='h-screen w-screen'>
+        <div className='h-100dvh'>
             <h1>Trying out GSAP observer</h1>
             <div>
                 <p>Mouse X = {mouse.x}</p>
                 <p>Mouse Y = {mouse.y}</p>
                 <p>Scroll value = {scroll}</p>
             </div>
-            <div className={`absolute -top-10 -left-10 h-20 w-20 flex items-center justify-center bg-red-500 rounded-full select-none z-100`} ref={circleRef}>
+            <div className={`absolute -top-10 -left-10 h-20 w-20 flex items-center justify-center bg-red-500 rounded-full pointer-events-none z-100`} ref={circleRef}>
                 Circle
             </div>
 
             <div ref={boxRef1} className="absolute left-[50dvw] top-[20dvh] h-20 w-20 bg-blue-600">
                 Box 1
             </div>
-            <div ref={boxRef2} className="absolute left-[40dvw] top-[40dvh] h-20 w-20 bg-green-600">
+            <div ref={boxRef2} className="absolute left-[40dvw] top-[40dvh] h-20 w-20 bg-yellow-600">
                 Box 2
             </div>
         </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,95 @@
+import gsap from "gsap";
 import type { Route } from "./+types/home";
+import { Observer } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { useState } from "react";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "React Router Playground" },
     { name: "description", content: "Play test area for various components and libraries." },
   ];
 }
 
+
+
+
+
 export default function Home() {
-  return(
-    <div className="h-screen w-screen">
+
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const screens = [0, 1, 2, 3, 4];
+  const screenLength = 5;
+
+  function ChangeScreen(direction: number) { // set direction +1 or -1 only
+    if (isAnimating) return;
+    setIsAnimating(true);
+
+    setIndex((current) => {
+      const next = (current + direction + screenLength) % screenLength;
+
+      console.log("Next index is", screens[next]);
+
+      gsap.to(`.screen${screens[current]}`, {
+        x: -100,
+        duration: 0.5,
+      });
+      gsap.to(`.screen${screens[next]}`, {
+        x: +100,
+        duration: 0.5,
+        onComplete: () => setIsAnimating(false),
+      });
+
+      return next;
+    });
+  }
+
+
+
+  useGSAP(() => {
+    gsap.to(".screen0", {
+      x: 100
+    })
+
+    gsap.registerPlugin(Observer);
+    Observer.create({
+      onUp: () => {
+        !isAnimating && ChangeScreen(1);
+        console.log("Scrolled up");
+      },
+      onDown: () => {
+        !isAnimating && ChangeScreen(-1);
+        console.log("Scrolled down");
+      }
+    })
+  })
+
+  return (
+    <div className="h-screen w-screen flex flex-col gap-10">
       Clean React Router Project
+      <div className="screen0 bg-blue-500 h-[10dvh] w-[10dvw]">
+        Screen 1
+      </div>
+
+      <div className="screen1 bg-blue-500 h-[10dvh] w-[10dvw]">
+        Screen 2
+      </div>
+
+      <div className="screen2 bg-blue-500 h-[10dvh] w-[10dvw]">
+        Screen 3
+      </div>
+
+      <div className="screen3 bg-blue-500 h-[10dvh] w-[10dvw]">
+        Screen 4
+      </div>
+
+      <div className="screen4 bg-blue-500 h-[10dvh] w-[10dvw]">
+        Screen 5
+      </div>
+
     </div>
+
   )
 }
