9f5b508e31e7c5ddbd814b361c2578eade2a9021
Ivin Mathew Kurian <xavenger2410@gmail.com>
2025-11-14 23:50:31 +0530

    Working on bevel effect

--- app/components/Screen0.tsx
+++ app/components/Screen0.tsx
@@ -1,11 +1,33 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import { Observer } from 'gsap/all';
 import React from 'react'
 
 type Props = {}
 
 const Screen0 = (props: Props) => {
+  gsap.registerPlugin(Observer);
+  useGSAP(() => {
+    {
+      Observer.create({
+        target: "disk",
+        type: "mouse",
+        onChangeX: () => {
+        },
+        onChangeY: () => {
+        }
+      })
+    }
+  })
   return (
-    <div className={`absolute screen0 h-screen w-screen bg-yellow-600`}>
-      This is screen 0.
+    /* This is the landing screen, so set up higher z so it always appears on top */
+    <div className={`absolute screen0 h-screen w-screen z-10`}>
+      <div className='h-full w-full flex flex-col items-center justify-center'>
+        <div className='disk bg-blue-600 h-40 w-100 rounded-xl p-10'>
+          Center of the screen
+        </div>
+      </div>
+
     </div>
   )
 }
